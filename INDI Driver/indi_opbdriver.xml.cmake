94b3ef7cba86bb41a9573261dface7d07f915980
Antiath <mispelaer_florent@hotmail.com>
2026-02-07 21:15:19 +0100

    Added Source code of the INDI Driver

---
+++ INDI Driver/indi_opbdriver.xml.cmake
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+   <devGroup group="Power">
+      <device label="Open Power Box" manufacturer="Generic">
+         <driver name="Open Power Box Driver">indi_opbdriver</driver>
+         <version>@CDRIVER_VERSION_MAJOR@.@CDRIVER_VERSION_MINOR@</version>
+      </device>
+   </devGroup>
+</driversList>
+
